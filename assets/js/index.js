bb848ea8f7e44a26e241dbc178e6e6bebe9b12a7
Jeremy May <may.jeremy.r@gmail.com>
2020-06-16 15:39:21 -0700

    sort by column header * Known bug: the first column sizing is weird

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,31 +32,104 @@
        2: {}
    ]
 */
-fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10')
-    .then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log('API fetch result: ', data);
-        createCoinTable(data);
-    }).catch(function (error) {
-        console.log(error);
-    });
+let lastSort = 'market_cap_desc';
+
+function getCoinData(sortBy) {
+    let sortParam = '';
+    if (sortBy) {
+        lastSort = sortBy;
+        sortParam = `&order=${sortBy}`;
+    }
+    let url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10${sortParam}`;
+    console.log('api url: ', url);
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log('API fetch result: ', data);
+            createCoinTable(data);
+        }).catch(function (error) {
+            console.log(error);
+        });
+}
+
+// live data
+getCoinData('market_cap_desc');
+
+// test data
+// fetch('/assets/js/test-data.json')
+//     .then(function (response) {
+//         return response.json();
+//     }).then(function (data) {
+//         console.log('Test data: ', data);
+//         createCoinTable(data);
+//     }).catch(function (error) {
+//         console.log(error);
+//     });
+
+const fmt = {
+    c: new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 2, maximumFractionDigits: 6}),
+    c0: new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0}),
+    n0: new Intl.NumberFormat('en-US', {style: 'decimal', maximumFractionDigits: 0}),
+    n2: new Intl.NumberFormat('en-US', {style: 'decimal', minimumFractionDigits: 2}),
+    p2: new Intl.NumberFormat('en-US', {style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2}),
+};
 
 async function createCoinTable(coinData) {
-    const coinTable = $('#coinTable');
+    console.log('creating coin table');
+    // clear out any old elements
+    $('tr.coin-row').remove();
+    const coinTable = $('#coinTable');    
 
     // create a table row for each coin in the list
     for (let key in coinData) {
         let coin = coinData[key];
-        console.log(`creating table row for: ${coin.name}`);
+        // console.log(`creating table row for: ${coin.name}`);
         coinTable.append(
-            $('<tr></tr>').append(
-                $('<td></td>').text(coin.market_cap_rank),
-                $('<td></td>').text(coin.symbol),
-                $('<td></td>').text(coin.name),
-                $('<td></td>').text(coin.market_cap),
-                $('<td></td>').text(coin.current_price)
+            $('<tr class="coin-row"></tr>').append(
+                $('<td class="text-left"></td>').text(coin.market_cap_rank),
+                $('<td class="text-left"></td>').append(
+                    $('<div></div>').append(
+                        `<img src="${coin.image}" width="16" height="16"> ${coin.name}`
+                    )),
+                $('<td ></td>').text(fmt.c0.format(coin.market_cap)),
+                $('<td></td>').text(fmt.c.format(coin.current_price)),
+                $('<td></td>').text(fmt.c0.format(coin.total_volume)),
+                $('<td></td>').text(fmt.n0.format(coin.circulating_supply)),
+                $('<td></td>').text(fmt.p2.format(coin.price_change_percentage_24h/100)),
+                $('<td></td>').text(''), // price graph?
             )
         );
     }
 }
+
+//sorting
+$('a.sortable-by-id').click(function() {
+    if (lastSort == 'id_asc') {
+        console.log('Sorting by name DESC');
+        getCoinData('id_desc')
+    } else {
+        console.log('Sorting by name ASC');
+        getCoinData('id_asc')
+    }    
+});
+
+$('a.sortable-by-market-cap').click(function() {
+    if (lastSort == 'market_cap_desc') {
+        console.log('Sorting by market cap ASC');
+        getCoinData('market_cap_asc')
+    } else {
+        console.log('Sorting by market cap DESC');
+        getCoinData('market_cap_desc')
+    }   
+});
+
+$('a.sortable-by-volume').click(function() {
+    if (lastSort == 'volume_asc') {
+        console.log('Sorting by volume DESC');
+        getCoinData('volume_desc')
+    } else {
+        console.log('Sorting by volume ASC');
+        getCoinData('volume_asc')
+    }    
+});
